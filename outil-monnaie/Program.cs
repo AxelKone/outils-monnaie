e1fe468f3d94f24de858a36700438775004389f3
AxelKone <axel.kone@lcieducation.net>
2023-09-28 10:53:22 -0400

    ajout projet

--- outil-monnaie/Program.cs
+++ outil-monnaie/Program.cs
@@ -1,29 +1,43 @@
 ﻿// Axelkone
 using System;
+
 class Program
 {
     static void Main()
     {
         // Taux de change actuels
-        double tauxUSD = 0.78; 
-        double tauxEuro = 0.68; 
+        double tauxUSD = 0.78; // 1 CAD = 0.78 USD
+        double tauxEuro = 0.68; // 1 CAD = 0.68 Euro
 
         Console.Write("Entrez un montant en dollars canadiens : ");
-
         if (double.TryParse(Console.ReadLine(), out double montantEnCAD))
         {
-            // Conversion en dollars américains 
-            double montantEnUSD = montantEnCAD * tauxUSD;
-            // Conversion en euros
-            double montantEnEuro = montantEnCAD * tauxEuro;
+            Console.Write("Choisissez la devise de conversion (USD/Euro) : ");
+            string choixDevise = Console.ReadLine();
 
-            // Affichage des résultats
-            Console.WriteLine($"{montantEnCAD} CAD équivaut à {montantEnUSD} USD");
-            Console.WriteLine($"{montantEnCAD} CAD équivaut à {montantEnEuro} Euro");
+            double montantConverti = 0.0;
+
+            // Effectuer la conversion en fonction du choix de l'utilisateur
+            switch (choixDevise.ToLower())
+            {
+                case "usd":
+                    montantConverti = montantEnCAD * tauxUSD;
+                    Console.WriteLine($"{montantEnCAD} CAD équivaut à {montantConverti} USD");
+                    break;
+                case "euro":
+                    montantConverti = montantEnCAD * tauxEuro;
+                    Console.WriteLine($"{montantEnCAD} CAD équivaut à {montantConverti} Euro");
+                    break;
+                default:
+                    Console.WriteLine("Choix de devise invalide. Veuillez choisir USD ou Euro.");
+                    break;
+            }
         }
-            Console.ReadKey();
+        else
+        {
+            Console.WriteLine("Montant invalide. Assurez-vous d'entrer un nombre valide.");
+        }
     }
-    
 }
 
 
