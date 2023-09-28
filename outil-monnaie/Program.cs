62c2c639f381264f82c1c46d8ee78362884f2f2d
AxelKone <axel.kone@lcieducation.net>
2023-09-28 10:38:34 -0400

    conversion CAD ==> USD et EUR

--- outil-monnaie/Program.cs
+++ outil-monnaie/Program.cs
@@ -1,5 +1,35 @@
 ﻿// Axelkone
-Console.WriteLine("Hello, World!");
+using System;
+class Program
+{
+    static void Main()
+    {
+        // Taux de change actuels
+        double tauxUSD = 0.78; 
+        double tauxEuro = 0.68; 
+
+        Console.Write("Entrez un montant en dollars canadiens : ");
+
+        if (double.TryParse(Console.ReadLine(), out double montantEnCAD))
+        {
+            // Conversion en dollars américains 
+            double montantEnUSD = montantEnCAD * tauxUSD;
+            // Conversion en euros
+            double montantEnEuro = montantEnCAD * tauxEuro;
+
+            // Affichage des résultats
+            Console.WriteLine($"{montantEnCAD} CAD équivaut à {montantEnUSD} USD");
+            Console.WriteLine($"{montantEnCAD} CAD équivaut à {montantEnEuro} Euro");
+        }
+            Console.ReadKey();
+    }
+    
+}
+
+
+
+
+
 
 
 
